ff2c4429875ea6ad505431c401f88f0ea2c4bae6
tbagrel1 <tomsb07@gmail.com>
2016-05-04 20:24:42 +0200

    Added files via upload

---
+++ archi_imbr/server/dict_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*
+
+score = {"pseudo": "Thomas",
+         "score": 3034,
+         "date": "28/04/2016",
+         "heure": "18:19",
+         "temps": 5,
+         "texte_m": "expl::1",
+         "texte_t": "Léo le cerf court dans la forêt. Il renc"}
