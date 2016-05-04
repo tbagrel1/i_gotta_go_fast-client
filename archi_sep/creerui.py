6ce0d20ec9ed9cf10a05c4e86806235d5014f1de
tbagrel1 <tomsb07@gmail.com>
2016-05-04 20:28:14 +0200

    V2

--- archi_sep/creerui.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*
-
-from os import system
-
-system("pyuic4 qt/FenetreBVN/fenetrebvn.ui > ui_fenetrebvn.py")
-system("pyuic4 qt/Menu/menu.ui > ui_menu.py")
-system("pyuic4 qt/Module/module.ui > ui_module.py")
