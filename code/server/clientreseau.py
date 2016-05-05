63c32c7139cb4c11a9ab435bb13b01105f6657f3
tbagrel1 <tomsb07@gmail.com>
2016-05-05 11:37:03 +0200

    v5

--- code/server/clientreseau.py
+++ code/server/clientreseau.py
@@ -3,7 +3,6 @@ import socket
 client_to_serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_to_serv.connect(("localhost", 25565))
 
-
 msg_recu = ""
 while msg_recu[-4:] != "\end":
     msg_recu += client_to_serv.recv(1024)
@@ -24,3 +23,72 @@ if msg_recu == "OK":
         del msg
 
     client_to_serv.close()
+
+class HandleScoreClient(object):
+    def __init__(self):
+        object.__init__()
+        self.suppr_score = False
+
+    def envoyerScore(self, score):
+        client_to_serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_to_serv.connect(("localhost", 25565))
+
+        msg_recu = ""
+        while msg_recu[-4:] != "\end":
+            msg_recu += client_to_serv.recv(1024)
+        msg_recu = msg_recu[:-4]
+        print(msg_recu)
+
+        if msg_recu == "OK":
+            client_to_serv.send(msg + "PUSH\end")
+
+            msg_recu = ""
+            while msg_recu[-4:] != "\end":
+                msg_recu += client_to_serv.recv(1024)
+            msg_recu = msg_recu[:-4]
+            print(msg_recu)
+            if msg_recu == "OK":
+
+                # On envoie le score
+                # client_to_serv.send(score)
+
+                msg_recu = ""
+                while msg_recu[-4:] != "\end":
+                    msg_recu += client_to_serv.recv(1024)
+                msg_recu = msg_recu[:-4]
+                print(msg_recu)
+
+                if msg_recu == "OK":
+                    self.suppr_score = True
+
+    def recevoirScore(self):
+        client_to_serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_to_serv.connect(("localhost", 25565))
+
+        msg_recu = ""
+        while msg_recu[-4:] != "\end":
+            msg_recu += client_to_serv.recv(1024)
+        msg_recu = msg_recu[:-4]
+        print(msg_recu)
+
+        if msg_recu == "OK":
+            client_to_serv.send(msg + "PULL\end")
+
+            msg_recu = ""
+            while msg_recu[-4:] != "\end":
+                msg_recu += client_to_serv.recv(1024)
+            msg_recu = msg_recu[:-4]
+            print(msg_recu)
+            if msg_recu == "OK":
+
+                # On envoie le score
+                # client_to_serv.send(score)
+
+                msg_recu = ""
+                while msg_recu[-4:] != "\end":
+                    msg_recu += client_to_serv.recv(1024)
+                msg_recu = msg_recu[:-4]
+                print(msg_recu)
+
+                if msg_recu == "OK":
+                    self.suppr_score = True
