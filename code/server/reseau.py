9ac51ea41b052cb790228d468601330033017d1e
tbagrel1 <tomsb07@gmail.com>
2016-05-05 11:17:06 +0200

    Répoonse

--- code/server/reseau.py
+++ code/server/reseau.py
@@ -1,64 +1,91 @@
+#!/usr/bin/python2
 # -*- coding: utf-8 -*
-#..Les Modules:
-#.Import du module socket afin de communiquer avec le clien 
-#.qui lui envera des informations sur les scores.
-import socket
-#.Import du modules pickle qui permet de recuperer ou 
-#.encoder sous forme chiffré brut une ligne de code ici en 
-#.l'ocurence on "pickle" les données du joueur qui son créé 
-#.sous forme de de liste impossible a recupere en l'etat, 
-#.c'est pourquoi on la transfor en "chiffré" signalant que 
-#.c'est un liste que l'on recupere.
-import pickle
 
+#.##Modules :
+#.Import du module socket afin de communiquer avec le client qui lui envera 
+#.des informations sur les scores.
+import socket
+#.Import du modules pickle qui permet de recuperer un objet python stocké dans 
+#.un fichier texte
+import pickle
+#.Import des modules pour la cryptographie
 import base64
 import os
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-#..Les fonctions:
-#.On recupere une ligne "pickle" correspondant au score, la 
-#.fonctions permet de rendre lisible les données que l'on 
-#.recupere sous forme texte et les passer sous forme python
-def unpick(msg):
-	doc_pick = open("pickle.txt", "wb")
-	doc_pick.write(msg)
-	return pickle.load(open("pickle.txt", "rb"))
 
+#.##Format de données reçues par le serveur
+#.On reçoit du client plusieurs chaînes de caractères correspondant à une 
+#.liste contenant le code de la fonction de cryptage, la checksum du fichier 
+#.qui appelle la fonction ainsi que le dictionnaire de score, le tout crypté
+
+#.##Les fonctions :
+#.On recupere une ligne "pickle" correspondant au score, la
+#.fonctions permet de rendre lisible les données que l'on
+#.recupere sous forme texte et les passer sous forme python
+
+
+def unpick(msg):
+    doc_pick = open("pickle.txt", "wb")
+    doc_pick.write(msg)
+    return pickle.load(open("pickle.txt", "rb"))
+
+#.Tout ce qui est dans ce bloc correspond au cryptage symétrique
+#.On prépare la clé Fernet qui permet de crypter et de décrypter
+#------------------------------------------------------------------------------
 mot_de_passe = "                "
+# cle_16o = os.urandom(16) --> Il faut que les deux parties utilisent la même
 cle_16o = "                "
+
 kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
                  length=32,
                  salt=cle_16o,
                  iterations=100000,
                  backend=default_backend())
-
 cle = base64.urlsafe_b64encode(kdf.derive(mot_de_passe))
 cle_fernet = Fernet(cle)
+#------------------------------------------------------------------------------
 
+#.On lance le service de connection
 serv_co = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv_co.bind(('',12345))
+#.On utilise le port `25565`
+serv_co.bind(('', 25565))
+#. On écoute un maximum d'une connection à la fois
 serv_co.listen(1)
+
+#.Tant que le serveur est en marche
 while True:
-	serv_to_client, infos_co = serv_co.accept()
-	print(infos_co)
-	msg_recu = ""
-	while msg_recu[-4:] != "\end":	
-		msg_recu += serv_to_client.recv(1024)
-	print(msg_recu)
-	msg_recu = msg_recu[:-4]
-	
-	fichier = open("message_crypte.txt", "wb")
-	fichier.write(msg_recu)
-	fichier.close()
-	
-	fichier = open("message_crypte.txt", "rb")
-	msg_crypte = fichier.read()
-	fichier.close()
-	msg_decrypte = cle_fernet.decrypt(msg_crypte)
-	msg_recu_final = unpick(msg_decrypte)
+    #.On accepte une connection et on récupère les informations de connection
+    (serv_to_client, infos_co) = serv_co.accept()
+    #.On affiche les informations de connection
+    print(infos_co)
+    #.On envoi un message au client pour dire qu'il est bien connecté
+    serv_to_client.send("OK\end")
 
-	serv_to_client.send("Ok !\end")
-	serv_to_client.close()
+    msg_recu = ""
+    #.Tant que le message n'est pas arrivé complétement
+    while msg_recu[-4:] != "\end":
+        #.On accepte les données (on vide le buffer)
+        msg_recu += serv_to_client.recv(1024)
+    #.On supprime le `\end` à la fin
+    msg_recu = msg_recu[:-4]
+    #.On affiche le message reçu
+    print(msg_recu)
+    #.On envoi un message au client pour dire que le score est bien arrivé
+    serv_to_client.send("OK\end")
+    serv_to_client.close()
 
+    # fichier = open("message_crypte.txt", "wb")
+    # fichier.write(msg_recu)
+    # fichier.close()
+
+    # fichier = open("message_crypte.txt", "rb")
+    # msg_crypte = fichier.read()
+    # fichier.close()
+    # msg_decrypte = cle_fernet.decrypt(msg_crypte)
+    # msg_recu_final = unpick(msg_decrypte)
+
+    # serv_to_client.send("Ok !\end")
+    # serv_to_client.close()
